b1bba976a635e6e926ef240ffe4301f0be995554
ph-kandy <70727213+ph-kandy@users.noreply.github.com>
2020-10-16 00:55:49 -0700

    cleaned up server.js and finished post/delete

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,21 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var jsonNoteData = require("./db/db.json");
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT ||3000;
+const{ v4: uuidv4 } = require("uuid");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname+"/public"))
+app.use(express.static(__dirname+"/public"));
 
 
 
-var notes = [];
+// var notes = [];
 
 
 // Routes
@@ -30,18 +32,37 @@ app.get("/", function(req, res) {
 
 // Displays Notes
 app.get("/api/notes", function(req, res) {
-  return res.json(db.json);
+  res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
 
 // Create the app.Post function for the note taker
 app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  var notesId = uuidv4();
+  newNote.id = notesId
+  jsonNoteData.push(newNote);
+
+fs.writeFile("./db/db.json", JSON.stringify(jsonNoteData), function(err){
+  if (err) throw err;
+  res.json("Success")
+});
 
 });
 
 // Create the app.delete to get rid of notes
-app.delete("/api/notes/:id", function(req, res){
-
+app.delete("/api/notes/:id", function(req,res) {
+  var jsonId = req.params.id
+  
+  for (var i = 0; i < jsonNoteData.length; i++) {
+      if (jsonNoteData[i].id === jsonId) {
+        jsonNoteData.splice(i,1);
+      }
+  }
+  fs.writeFile("./db/db.json", JSON.stringify(jsonNoteData), function(err) {
+      if (err) throw err;
+      res.json("Success")
+  })
 });
 
 // Starts the server to begin listening
